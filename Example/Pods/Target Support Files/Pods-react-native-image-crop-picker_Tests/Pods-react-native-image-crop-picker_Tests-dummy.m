f690a2c0ec33269e6cfec5e4c294a786e52b2be0
RichieZhl <lylaut@163.com>
2017-11-03 14:15:07 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-react-native-image-crop-picker_Tests/Pods-react-native-image-crop-picker_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_react_native_image_crop_picker_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_react_native_image_crop_picker_Tests
+@end
